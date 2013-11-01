e62581614ec574956e1de3d8e239bf8ceb22f7e9
Chris Love <clove@extremewebworks.com>
2013-11-01 12:44:14 -0400

    adding soem rwd to the theater calendar adding theater files

---
+++ Chapter 15 - Offlining the Web/Offline/Web Site/js/debug/movie.app.theater-view.js
@@ -0,0 +1,34 @@
+﻿/// <reference path="love2dev.app.js" />
+/// <reference path="love2dev.app.api.js" />
+;
+
+(function (window, undefined) {
+
+    "use strict";
+
+    movieApp.fn.cleanFirstPoster = function (data) {
+
+
+        return data;
+    };
+
+    movieApp.fn.loadTheatersView = function (params) {
+
+        var that = this;
+
+        that.setMainTitle(decodeURIComponent(params.theaterName));
+
+        that.InTheatersMovies(50, 1, function (data) {
+
+            if (!data) {
+                return;
+            }
+
+            that.mergeData(".movie-showtimes-wrapper", "MovieStartTimesTemplate",
+                that.cleanFirstPoster(data));
+
+        });
+
+    }
+
+}());
