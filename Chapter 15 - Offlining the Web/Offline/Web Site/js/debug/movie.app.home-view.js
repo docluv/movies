42c7d81c3a4ab57e491571dbfe793350c6c1f255
Chris Love <clove@extremewebworks.com>
2013-12-08 22:02:49 -0500

    changed the homeView to a pure controller

--- Chapter 15 - Offlining the Web/Offline/Web Site/js/debug/movie.app.home-view.js
+++ Chapter 15 - Offlining the Web/Offline/Web Site/js/debug/movie.app.home-view.js
@@ -6,103 +6,6 @@
 
     "use strict";
 
-    movieApp.fn.unloadHomeView = function () {
-        delete this.resizeEvents["manageHomeView"];
-        this.panorama = undefined;
-    };
-
-    movieApp.fn.renderHomeMovies = function (target, data) {
-
-        if (!data) {
-            return;
-        }
-
-        this.mergeData(target, "MoviePosterGridTemplate", data);
-        this.setPosterSrc();
-
-    };
-
-    movieApp.fn.loadHomeView = function () {
-
-        var that = this,
-            i = 0,
-            vPanels = document.querySelectorAll(".panel-v-scroll");
-
-        that.setupPanorama();
-        that.setMainTitle("Modern Web Movies");
-
-        that.InTheatersMovies(50, 1, function (data) {
-            that.renderHomeMovies.call(that, ".top-box-list", data);
-        });
-
-        that.OpeningMovies(50, 1, function (data) {
-            that.renderHomeMovies.call(that, ".opening-movie-list", data);
-        });
-
-        that.TopBoxOfficeMovies(50, 1, function (data) {
-            that.renderHomeMovies.call(that, ".movies-near-me-list", data);
-        });
-
-        that.CommingSoonMovies(50, 1, function (data) {
-            that.renderHomeMovies.call(that, ".comming-soon-list", data);
-        });
-
-        that.setPanoramaWidth();
-
-        that.setupMQL("min600", "(min-width: 600px)", [{
-            matchName: "manageHomeView",
-            matchFunc: function () {
-                that.setPanoramaWidth.call(that);
-                that.setPosterSrc.call(that);
-            },
-            nomatchName: "manageHomeView",
-            nomatchFunc: function () {
-                that.setPanoramaWidth.call(that);
-                that.setPosterSrc.call(that);
-            }
-        }]);
-
-        that.setupMQL("min1024", "(min-width: 1024px)", [{
-            matchName: "manageHomeView1024",
-            matchFunc: function () {
-                that.setPanoramaWidth.call(that);
-                that.setPosterSrc.call(that);
-            },
-            nomatchName: "manageHomeView1024",
-            nomatchFunc: function () {
-                that.setPanoramaWidth.call(that);
-                that.setPosterSrc.call(that);
-            }
-        }]);
-
-    };
-
-    movieApp.fn.viewWidth = window.innerWidth;
-
-    movieApp.fn.setPanoramaWidth = function () {
-
-        var that = this,
-            i = 0,
-            peekWidth = (that.viewWidth > 600) ? 50 : 30,
-            panelWidth = (that.viewWidth - peekWidth),
-            panoramaWrapper = document.querySelector(".panorama-panels"),
-            panels = document.querySelectorAll(".single-panel"),
-            movieGrids = document.querySelectorAll(".movie-poster-grid"),
-            gridHeight = getGridHeight(parseInt(panoramaWrapper.style.height, 10));
-
-        for (; i < panels.length; i++) {
-            panels[i].style.width = panelWidth + "px";
-        }
-
-        if (panoramaWrapper) {
-            panoramaWrapper.style.width = (panels.length * panelWidth) + "px";
-        }
-
-        that.panorama.resizePanorama();
-
-    };
-
-
     function getGridHeight(baseHeight) {
 
         var wWidth = window.innerWidth;
@@ -118,4 +21,104 @@
 
     };
 
+    movieApp.fn.homeView = {
+
+        onload: function () {
+
+            var that = this,
+                hv = that.homeView,
+                i = 0,
+                vPanels = document.querySelectorAll(".panel-v-scroll");
+
+            that.setupPanorama();
+            that.setMainTitle("Modern Web Movies");
+
+            that.InTheatersMovies(50, 1, function (data) {
+                hv.renderHomeMovies.call(that, ".top-box-list", data);
+            });
+
+            that.OpeningMovies(50, 1, function (data) {
+                hv.renderHomeMovies.call(that, ".opening-movie-list", data);
+            });
+
+            that.TopBoxOfficeMovies(50, 1, function (data) {
+                hv.renderHomeMovies.call(that, ".movies-near-me-list", data);
+            });
+
+            that.CommingSoonMovies(50, 1, function (data) {
+                hv.renderHomeMovies.call(that, ".comming-soon-list", data);
+            });
+
+            hv.setPanoramaWidth.call(that);
+
+            that.setupMQL("min600", "(min-width: 600px)", [{
+                matchName: "manageHomeView",
+                matchFunc: function () {
+                    hv.setPanoramaWidth.call(that);
+                    that.setPosterSrc.call(that);
+                },
+                nomatchName: "manageHomeView",
+                nomatchFunc: function () {
+                    hv.setPanoramaWidth.call(that);
+                    that.setPosterSrc.call(that);
+                }
+            }]);
+
+            that.setupMQL("min1024", "(min-width: 1024px)", [{
+                matchName: "manageHomeView1024",
+                matchFunc: function () {
+                    hv.setPanoramaWidth.call(that);
+                    that.setPosterSrc.call(that);
+                },
+                nomatchName: "manageHomeView1024",
+                nomatchFunc: function () {
+                    hv.setPanoramaWidth.call(that);
+                    that.setPosterSrc.call(that);
+                }
+            }]);
+
+        },
+
+        //unload: function () {
+        //  //  delete this.resizeEvents["manageHomeView"];
+        //},
+
+        renderHomeMovies : function (target, data) {
+
+            if (!data) {
+                return;
+            }
+
+            this.mergeData(target, "MoviePosterGridTemplate", data);
+            this.setPosterSrc();
+
+        },
+
+        viewWidth : window.innerWidth,
+
+        setPanoramaWidth : function () {
+
+            var that = this,
+                i = 0,
+                peekWidth = (that.viewWidth > 600) ? 50 : 30,
+                panelWidth = (that.viewWidth - peekWidth),
+                panoramaWrapper = document.querySelector(".panorama-panels"),
+                panels = document.querySelectorAll(".single-panel"),
+                movieGrids = document.querySelectorAll(".movie-poster-grid"),
+                gridHeight = getGridHeight(parseInt(panoramaWrapper.style.height, 10));
+
+            for (; i < panels.length; i++) {
+                panels[i].style.width = panelWidth + "px";
+            }
+
+            if (panoramaWrapper) {
+                panoramaWrapper.style.width = (panels.length * panelWidth) + "px";
+            }
+
+            that.panorama.resizePanorama();
+
+        }
+
+    };
+    
 }(window));
