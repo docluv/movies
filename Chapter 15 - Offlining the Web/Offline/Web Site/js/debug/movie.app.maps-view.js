b622d3e861c594178d1dc8cee6607fa3e3f810f4
Chris Love <clove@extremewebworks.com>
2013-12-08 20:00:47 -0500

    changing mapsview to view controller

--- Chapter 15 - Offlining the Web/Offline/Web Site/js/debug/movie.app.maps-view.js
+++ Chapter 15 - Offlining the Web/Offline/Web Site/js/debug/movie.app.maps-view.js
@@ -1,8 +1,11 @@
 ﻿/// <reference path="movie.app.js" />
 /// <reference path="http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0" />
 
+(function (window, undefined) {
 
-var fakeTheaters = ["The Mystic",
+    "use strict";
+
+    var fakeTheaters = ["The Mystic",
                     "The Marquee",
                     "The Pantagees",
                     "Regal 22",
@@ -12,90 +15,99 @@ var fakeTheaters = ["The Mystic",
                     "Regal Pointe Stadium 20 & IMAX",
                     "Touchstar Cinemas Southchase 7",
                     "Cinemark Festival Bay Mall"
-                    ];
+    ];
 
-movieApp.fn.loadMapsView = function () {
+    movieApp.fn.mapsView = {
 
-    var that = this,
-        title = "nearby";
+        onload: function () {
 
-    $.loadScript("bing-map-script", "http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0", function () {
+            var that = this,
+                mv = that.mapsView,
+                title = "nearby";
 
-        that.loadNearbyTheaters();
+            $.loadScript("bing-map-script", "http://ecn.dev.virtualearth.net/mapcontrol/mapcontrol.ashx?v=7.0", function () {
 
-    }, "head");
+                mv.loadNearbyTheaters.call(that);
 
-};
+            }, "head");
 
-movieApp.fn.loadNearbyTheaters = function () {
+        },
 
-    var that = this,
-        map, i,
-        key = "Ai2HABTSdPR3baDe6yPjFDNRac3RsbaFMjUb2d-OjlQd8o3vO2DcqRxBpDgRTuUD";
+        loadNearbyTheaters: function () {
 
-    if (navigator.geolocation) {
+            var that = this,
+                mv = that.mapsView,
+                map, i,
+                key = "Ai2HABTSdPR3baDe6yPjFDNRac3RsbaFMjUb2d-OjlQd8o3vO2DcqRxBpDgRTuUD";
 
-        navigator.geolocation.getCurrentPosition(function (position) {
+            if (navigator.geolocation) {
 
-            map = new Microsoft.Maps.Map(document.getElementById('nearbyMap'), {
-                credentials: key,
-                center: new Microsoft.Maps.Location(position.coords.latitude,
-                                                    position.coords.longitude),
-                mapTypeId: Microsoft.Maps.MapTypeId.road,
-                showScalebar: false,
-                showDashboard: false,
-                zoom: 12
-            });
+                navigator.geolocation.getCurrentPosition(function (position) {
 
-            //loop thorugh and place 10 random pushpins to represent theaters on the map
-            for (i = 0; i < 10; i++) {
-                that.AddPushpin.call(that, position, map, fakeTheaters[i]);
+                    map = new Microsoft.Maps.Map(document.getElementById('nearbyMap'), {
+                        credentials: key,
+                        center: new Microsoft.Maps.Location(position.coords.latitude,
+                                                            position.coords.longitude),
+                        mapTypeId: Microsoft.Maps.MapTypeId.road,
+                        showScalebar: false,
+                        showDashboard: false,
+                        zoom: 12
+                    });
+
+                    //loop thorugh and place 10 random pushpins to represent theaters on the map
+                    for (i = 0; i < 10; i++) {
+                        mv.AddPushpin.call(that, position, map, fakeTheaters[i]);
+                    }
+
+                });
+
             }
 
-        });
+        },
 
-    }
+        AddPushpin: function (position, map, theaterName) {
 
-};
+            var that = this,
+                mv = that.mapsView,
+                  shape,
+                  coords = mv.getRandomPoistion(position),
+                  pin = new Microsoft.Maps.Pushpin(coords /*, { text: theaterName }*/);
 
-movieApp.fn.AddPushpin = function (position, map, theaterName) {
+            // Add a handler to the pushpin drag
+            Microsoft.Maps.Events.addHandler(pin, 'click', function () {
+                mv.goToTheaterFromMap(theaterName);
+            });
 
-    var that = this,
-          shape,
-          coords = that.getRandomPoistion(position),
-          pin = new Microsoft.Maps.Pushpin(coords /*, { text: theaterName }*/);
+            map.entities.push(pin);
+        },
 
-    // Add a handler to the pushpin drag
-    Microsoft.Maps.Events.addHandler(pin, 'click', function () {
-        that.goToTheaterFromMap(theaterName);
-    });
+        goToTheaterFromMap: function (theaterName) {
+            window.location.hash = "#!theater/" + theaterName;
+        },
 
-    map.entities.push(pin);
-};
+        getRandomPoistion: function (position) {
 
-movieApp.fn.goToTheaterFromMap = function (theaterName) {
-    window.location.hash = "#!theater/" + theaterName;
-}
+            var r = Math.floor((Math.random() * 5000) + 1) / 100000,
+                  rl = Math.floor((Math.random() * 5500) + 1) / 100000,
+                  pm = Math.round(Math.random()),
+                  pm1 = Math.round(Math.random()),
+                  coords = {};
 
-movieApp.fn.getRandomPoistion = function (position) {
+            if (pm === 0) {
+                r = parseFloat("-" + r);
+            }
 
-    var r = Math.floor((Math.random() * 5000) + 1) / 100000,
-          rl = Math.floor((Math.random() * 5500) + 1) / 100000,
-          pm = Math.round(Math.random()),
-          pm1 = Math.round(Math.random()),
-          coords = {};
+            if (pm1 === 0) {
+                rl = parseFloat("-" + rl);
+            }
 
-    if (pm === 0) {
-        r = parseFloat("-" + r);
-    }
+            coords.latitude = position.coords.latitude + r;
+            coords.longitude = position.coords.longitude + rl;
 
-    if (pm1 === 0) {
-        rl = parseFloat("-" + rl);
-    }
+            return coords;
+        }
 
-    coords.latitude = position.coords.latitude + r;
-    coords.longitude = position.coords.longitude + rl;
+    };
 
-    return coords;
-};
+}(window));
 
