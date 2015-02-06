2baed158993fa24b04551217e0d5a0b0568bd9fc
Chris Love <clove@extremewebworks.com>
2015-02-06 15:12:02 -0500

    fixed bug in the bundled script

--- Modern Movie Web/Web/Web Site/js/debug/views/movie.app.about-view.js
+++ Modern Movie Web/Web/Web Site/js/debug/views/movie.app.about-view.js
@@ -6,20 +6,8 @@
 
         onload: function () {
 
-            
-
-            console.log("load about");
             this.setMainTitle("about modern web movies");
-        },
-
-        unload: function () {
-
-            console.log("unload about");
-
-        },
-
-        $rootScope: undefined,
-        params: undefined,
+        }
 
     };
 
